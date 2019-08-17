e0c0a0f5aff1dd3afc4e7fa6ed2650f013faa5ba
Echim2016 <ychxz26@gmail.com>
2019-08-17 21:20:53 +0800

    new topic categories updated

--- js/index1.js
+++ js/index1.js
@@ -187,6 +187,9 @@ function getQuote() {
       if(featuredArray[randomquote]==3){
         currentText = "本月編輯精選" ;
       }
+      else if (featuredArray[randomquote]==4){
+        currentText = "精彩絕倫系列" ;
+      }
       else if (featuredArray[randomquote]==2) {
         currentText = "歷久不衰系列"
       }
